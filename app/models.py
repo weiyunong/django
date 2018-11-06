22eb663c0974fc8426318641d162d7d6b8c44e44
weiyunong <1411526831@qq.com>
2018-11-05 17:48:35 -0800

    登录注册轮播图完成

--- app/models.py
+++ app/models.py
@@ -7,9 +7,5 @@ class User(models.Model):
     password = models.CharField(max_length=50)
     token = models.CharField(max_length=256)
 
-# 轮播图　模型类
-class Wheel(models.Model):
-    id = models.IntegerField(primary_key=True)
-    img = models.CharField(max_length=100)
 
 
